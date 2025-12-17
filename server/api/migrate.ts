7d964221762281c2576d9690051f02559ccc41ab
Hugo Richard <hugo.richard@vercel.com>
2025-12-17 11:04:35 +0000

    test

--- server/api/migrate.ts
+++ server/api/migrate.ts
@@ -1,11 +1,20 @@
+import { fileURLToPath } from 'node:url'
 import { applyDatabaseMigrations, applyDatabaseQueries } from '@nuxthub/core/db'
 import { db } from 'hub:db'
+import { dirname, resolve } from 'pathe'
 
 export default defineEventHandler(async () => {
   const { hub } = useRuntimeConfig()
 
-  await applyDatabaseMigrations(hub as any, db)
-  await applyDatabaseQueries(hub as any, db)
+  const serverDir = dirname(fileURLToPath(import.meta.url))
+
+  const prodHubConfig = {
+    ...hub,
+    dir: resolve(serverDir, '..')
+  }
+
+  await applyDatabaseMigrations(prodHubConfig as any, db)
+  await applyDatabaseQueries(prodHubConfig as any, db)
 
   return { result: 'Migrations applied' }
 })
